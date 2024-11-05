51c8ba17e5aa768a3af2e867c2098722296a019c
Khemmie-Ray <atokemmy@gmail.com>
2024-11-05 02:25:38 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import {
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./layout/Layout";
+import { config } from "./constants/config";
+import { WagmiProvider } from "wagmi";
 import { configWeb3Modal } from "./connection";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 configWeb3Modal()
 
@@ -19,10 +22,22 @@ const router = createBrowserRouter(createRoutesFromElements(
     </Route>
   </Route>));
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+    },
+  },
+});
+
 const App = () => {
   return (
     <div className="max-w-[1440px] mx-auto font-openSans">
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
+        </QueryClientProvider>
+      </WagmiProvider>
     </div>
   )
 };
